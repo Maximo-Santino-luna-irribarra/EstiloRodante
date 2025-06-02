c406034abfc37e322a0fa63b45f34f4c0f26e976
Santy_M <santy15021502@gmail.com>
2025-06-02 15:57:47 -0300

    Cambios Home

---
+++ Front/Scrpits/home.js
@@ -0,0 +1,33 @@
+const products = document.querySelectorAll("#productCarousel > div");
+const upBtn = document.getElementById("btnUp");
+const downBtn = document.getElementById("btnDown");
+
+let currentStart = 0;
+const VISIBLE_COUNT = 3;
+
+function renderProducts() {
+products.forEach((div, index) => {
+    div.style.display = (index >= currentStart && index < currentStart + VISIBLE_COUNT) ? "flex" : "none";
+});
+}
+
+upBtn.addEventListener("click", () => {
+if (currentStart - VISIBLE_COUNT >= 0) {
+    currentStart -= VISIBLE_COUNT;
+    renderProducts();
+}else{
+    alert("Ya estas en el inicio")
+}
+});
+
+downBtn.addEventListener("click", () => {
+if (currentStart + VISIBLE_COUNT < products.length) {
+    currentStart += VISIBLE_COUNT;
+    renderProducts();
+}else{
+    alert("No hay mas productos")
+}
+});
+
+renderProducts();
+
