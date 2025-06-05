7e2b24730275fe5ac32f83dded69e78e200ef751
Santy_M <santy15021502@gmail.com>
2025-06-05 14:40:16 -0300

    advance products js

--- Front/Scrpits/products.js
+++ Front/Scrpits/products.js
@@ -21,3 +21,24 @@ const products = document.querySelectorAll(".prod")
 for (let i = 0; i < products.length; i++) {
     console.log(products[i].value)
 }
+
+const insertProducts = () =>{
+    fetch('/neumaticos_50.json')
+    .then(res =>{
+        return res.json()
+    })
+    .then(productos =>{
+        for (let i = 0; i < productos.length; i++) {
+            writeProduct(productos[i]['marca'] , productos[i]['modelo'], productos[i]['medida'], productos[i]["indice_de_carga"], productos[i]["indice_de_velocidad"], productos[i]["precio"], productos[i]["caracteristicas"]['tecnologias'])
+        }
+    })
+}
+
+const writeProduct = (marca, modelo, medida, indiceCarga, indiceVelocidad, precio, tecnologias)=>{
+    let product = `
+    
+    `
+    console.log(marca, modelo, medida, indiceCarga, indiceVelocidad, precio, tecnologias)
+}
+
+insertProducts()
